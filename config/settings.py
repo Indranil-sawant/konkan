77a6bc1eaf42329d437ab404c789035f01cc33a4
Indranil-sawant <indranilsawant07@gmail.com>
2026-05-14 09:01:42 +0000

    Improve mobile UI, add navigation links, integrate Cloudinary, and update project configuration

--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third-party
+    'cloudinary_storage',
+    'cloudinary',
     'rest_framework',
     'django_filters',
-    'storages',
 
     # Local apps
     'core',
@@ -252,45 +253,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STORAGES = {
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
 
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
-
-# ------------------------------------------------------------------------------
-# SUPABASE S3 STORAGE
-# ------------------------------------------------------------------------------
-
-if os.environ.get('AWS_ACCESS_KEY_ID'):
-
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
-    AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
-
-    AWS_S3_REGION_NAME = os.environ.get(
-        'AWS_S3_REGION_NAME',
-        'us-east-1'
-    )
-
-    AWS_S3_SIGNATURE_VERSION = 's3v4'
-
-    AWS_S3_FILE_OVERWRITE = False
-
-    AWS_QUERYSTRING_AUTH = False
-
-    AWS_DEFAULT_ACL = None
-
-    # Use Supabase S3 for media uploads
-    STORAGES["default"] = {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    }
 
 # ------------------------------------------------------------------------------
 # DEFAULT PRIMARY KEY
